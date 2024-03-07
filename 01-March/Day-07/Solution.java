bf84807f0160f3888b4592d53583af65055c7d41
Manoj Bhavvan <manojbhavvan124@gmail.com>
2024-03-07 21:56:56 +0530

    Day-07
    Added "Middle of the Linked List"

---
+++ 01-March/Day-07/Solution.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode middle = head;
+        int count = 1;
+        while (head.next != null) {
+            head = head.next;
+            count++;
+            if (count % 2 == 0) {
+                middle = middle.next;
+            }
+        }
+        return middle;
+    }
+}
